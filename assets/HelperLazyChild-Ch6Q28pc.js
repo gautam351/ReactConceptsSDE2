ed06acfeadf656593718f2f3f5577b4417b8c2c5
gautam351 <89213502+gautam351@users.noreply.github.com>
2025-11-07 18:46:10 +0000

    Deploying to gh-pages from @ gautam351/ReactConceptsSDE2@a1e438226f17ccb4ddea6d653032f7716bee2116 🚀

---
+++ assets/HelperLazyChild-Ch6Q28pc.js
@@ -0,0 +1,1 @@
+import{r as s,j as e}from"./index-OmJKlIJq.js";const a=()=>{const r=s.useRef("../../../public/PublishMedium.jpeg");return e.jsx(e.Fragment,{children:e.jsx("img",{src:r.current,alt:"Lazy Loaded"})})};export{a as default};
