3f75ea951f160152e04eb42a20aff2fe7732f5ca
gautam351 <gautampraveen351@gmail.com>
2025-11-08 00:05:35 +0530

    github action added main branch

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ReactConceptsSDE2/',  // Set the base path for GitHub Pages deployment
   plugins: [
+
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
